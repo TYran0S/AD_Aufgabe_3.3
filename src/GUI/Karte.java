e9935edbfdd1e213b3390f8c4b044a7fa3587a89
TYran0S <inonskhay@gmail.com>
2013-05-31 22:36:14 +0200

    connections werden korrekt angezeigt

--- src/GUI/Karte.java
+++ src/GUI/Karte.java
@@ -261,6 +261,7 @@ public class Karte extends Application implements View {
                                             tmpCitylist.add(selectedLabels.get(1));
                                             Connection connection = new Connection(selectedLabels.get(0), distance, 0, tmpCitylist);
                                             nodes.get(selectedLabels.get(0)).trails.add(connection);
+                                            nodes.get(selectedLabels.get(1)).trails.add(connection);
                                             customconnections.add(connection);
                                             System.out.println("labelId: " + label.getId());
                                             cityLabel.get(selectedLabels.get(0)).setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/resource/haus_symbol_small.jpg"))));
