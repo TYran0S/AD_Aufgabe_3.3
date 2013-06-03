2cf8aba3870c6a5fe1683f08698a22ecad0bf661
jonestheman <jones.leetman@googlemail.com>
2013-06-03 21:05:50 +0200

    20/penta fix

--- src/aufgabe3_1/Simulation.java
+++ src/aufgabe3_1/Simulation.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import javax.swing.JOptionPane;
+
 /**
  * Immutable Klasse des Simulationsablaufs
  * 
@@ -19,18 +21,21 @@ public class Simulation {
 
     final public int CITIES; // Anzahl der Staedte
     final public String TESTDATA; // TSP Datei
-    final public static int capacity = 2;
-    final public static int ANTS = 50; // Anzahl der Ameisen
+    public static int capacity;
+    final public static int ANTS = 1; // Anzahl der Ameisen
     final public static int STARTNODE = 1; // Startpunkt der Ameise
     final public static double ALPHA = 1; // Einfluss der Pheromonene
     final public static double BETA = 5.0; // Einfluss der Weglaenge
     final public static double RHO = 0.5; // Evaporationskonstante
     final public static double Q = 1.0; // Pheromon-Menge eines
-                                        // Connection-Updates
+    // Connection-Updates
+	public static List<List<Integer>> bestTours;
     final public static int MAXCYCLES = 500000; // Maximale schritte der
-                                                // Simulation
+    // Simulation
     public static final int MAXHISTORY = 20; // maximum der History der
-                                             // gefundenen Wege
+    // gefundenen Wege
+
+    public static int BESTNUMBEROFDELIVERYVANS = 0x7FFFFFFF;//speichert die beste gefundene Anzahl von benoetigten Lieferwagen 
 
     final public List<Connection> CONNECTIONS;
     final public List<Ant> ANTLIST;
@@ -43,72 +48,102 @@ public class Simulation {
         List<List<Integer>> t = new ArrayList<List<Integer>>();
         t.add(new ArrayList<Integer>());
         t.add(new ArrayList<Integer>());
-        t.get(0).add(5);
-        t.get(0).add(2);
-        t.get(0).add(3);
-        t.get(0).add(4);
-        t.get(0).add(6);
-        t.get(1).add(1);
-        t.get(1).add(1);
-        t.get(1).add(1);
-        t.get(1).add(1);
-        t.get(1).add(1);
-
+//        t.get(0).add(5);
+//        t.get(0).add(2);
+//        t.get(0).add(3);
+//        t.get(0).add(4);
+//        t.get(0).add(6);
+//        t.get(1).add(2);
+//        t.get(1).add(2);
+//        t.get(1).add(3);
+//        t.get(1).add(0);
+//        t.get(1).add(3);
+//        capacity = 8;
+        TESTDATA = path;
+        List<Connection> connections = Parser.initConnections(Parser.parseTestFile(TESTDATA));// Connections
+        // parsen
+        // aus
+        // der
+        // Testdatei
+		for (int i = 0; i < Parser.packetList.size(); i++) {
+			t.get(0).add(i + 2);
+			t.get(1).add(Parser.packetList.get(i));
+        }
+		
         ACOImpl.ACOImplInit(t);
         
         CITIES = cities;
-        TESTDATA = path;
+      
         COLONY = new ACOImpl();
-        List<Connection> connections = Parser.initConnections(Parser.parseTestFile(TESTDATA));// Connections
-                                                                                              // parsen
-                                                                                              // aus
-                                                                                              // der
-                                                                                              // Testdatei
-
+       
         // Connections durch evaporate-Methode mit positivem Pheromon-Wert
         // vorinitialisieren, um bei Wahrscheinlichkeitsberechnung nicht durch 0
         // zu dividieren
         connections = COLONY.evaporate(connections, Q);
         NODES = Parser.initNodes(connections);// Geupdatete Connections in die
-                                              // Liste der Nodes erzeugen
+        // Liste der Nodes erzeugen
 
         CONNECTIONS = connections;
         ANTLIST = COLONY.createAnts(ANTS, NODES.get(STARTNODE - 1));// Kolonie
-                                                                    // von
-                                                                    // Ameisen
-                                                                    // erstellen
+        // von
+        // Ameisen
+        // erstellen
         BESTROUTE = new LinkedList<List<Integer>>();
         STEPS = 0;
     }
+    public Simulation(int cities, List<Connection> con, List<Node> nod, List<List<Integer>> t, int capa) {
 
-    private Simulation(int cities, String path, List<Connection> cons, List<Ant> antlist, List<Node> nodes, List<List<Integer>> bestRoute, int steps) {
-        List<List<Integer>> t = new ArrayList<List<Integer>>();
-        t.add(new ArrayList<Integer>());
-        t.add(new ArrayList<Integer>());
-        t.get(0).add(5);
-        t.get(0).add(2);
-        t.get(0).add(3);
-        t.get(0).add(4);
-        t.get(0).add(6);
-        t.get(1).add(1);
-        t.get(1).add(1);
-        t.get(1).add(1);
-        t.get(1).add(1);
-        t.get(1).add(1);
-
-        ACOImpl.ACOImplInit(t);
+            
+            ACOImpl.ACOImplInit(t);
+            this.capacity = capa;
+            TESTDATA = "werd glücklich java";
+            CITIES = cities;
+            COLONY = new ACOImpl();
+            // Connections durch evaporate-Methode mit positivem Pheromon-Wert
+            // vorinitialisieren, um bei Wahrscheinlichkeitsberechnung nicht durch 0
+            // zu dividieren
+            con = COLONY.evaporate(con, Q);
+            NODES = nod;// Geupdatete Connections in die
+        // Liste der Nodes erzeugen
         
-        CITIES = cities;
-        TESTDATA = path;
-        COLONY = new ACOImpl();
-        List<Connection> connections = cons;
-        connections = COLONY.evaporate(connections, Q);
-        NODES = nodes;
-        CONNECTIONS = connections;
-        ANTLIST = antlist;
-        BESTROUTE = bestRoute;
-        STEPS = steps;
+            CONNECTIONS = con;
+            ANTLIST = COLONY.createAnts(ANTS, NODES.get(STARTNODE - 1));// Kolonie
+        // von
+        // Ameisen
+        // erstellen
+        BESTROUTE = new LinkedList<List<Integer>>();
+            STEPS = 0;
     }
+
+        private Simulation(int cities, String path, List<Connection> cons, List<Ant> antlist, List<Node> nodes, List<List<Integer>> bestRoute, int steps) {
+            List<List<Integer>> t = new ArrayList<List<Integer>>();
+            t.add(new ArrayList<Integer>());
+            t.add(new ArrayList<Integer>());
+            System.out.printf("init\n");
+            t.get(0).add(5);
+            t.get(0).add(2);
+            t.get(0).add(3);
+            t.get(0).add(4);
+            t.get(0).add(6);
+            t.get(1).add(1);
+            t.get(1).add(1);
+            t.get(1).add(1);
+            t.get(1).add(1);
+            t.get(1).add(1);
+            capacity = 8;
+            ACOImpl.ACOImplInit(t);
+
+            CITIES = cities;
+            TESTDATA = path;
+            COLONY = new ACOImpl();
+            List<Connection> connections = cons;
+            connections = COLONY.evaporate(connections, Q);
+            NODES = nodes;
+            CONNECTIONS = connections;
+            ANTLIST = antlist;
+            BESTROUTE = bestRoute;
+            STEPS = steps;
+        }
 
     /**
      * Laesst dieses Simulationsobjekt die naechsten schritte errechnen und gibt
@@ -131,7 +166,7 @@ public class Simulation {
         if (!BESTROUTE.isEmpty()) {
             bestLength = COLONY.length(BESTROUTE.get(0), connections);
         }
-        
+
         for (int i = 1; (i <= steps) && steps < MAXCYCLES;i++) {
             System.out.printf("Step = %d/%d\n", i,steps);
             for (int j = 0; j < antList.size(); j++) {
@@ -156,7 +191,7 @@ public class Simulation {
                 Connection tempConnection = null;
                 for (Connection element : connections) {
                     if (element.cities.containsAll(ant.visitedNodes.subList(ant.visitedNodes.size() - 2,
-                            ant.visitedNodes.size() - 1))) {
+                                    ant.visitedNodes.size() - 1))) {
                         tempConnection = element;
                     } // if
                 } // for
@@ -166,27 +201,26 @@ public class Simulation {
                 connections.set((tempConnection.ID - 1), COLONY.updatePheromones(tempConnection, Q));
 
                 // Nodes mit aktualisierten Connections erzeugen
-                nodes = Parser.initNodes(connections);
-                
+                //TODO nodes = Parser.initNodes(connections);
+
                 // Entladen der Packete wenn moeglich
                 ant.unload(nextNode.ID);
                 ant.load(nextNode.ID);
-                
+
 
                 // bist du fertig ?
                 if (COLONY.tourFinished(ant, nextNode.ID)) {
-                    int tempLength = COLONY.length(ant.visitedNodes, connections); // Laenge
-                                                                                              // berechnen
+                    System.out.printf("Ant %d finish tour\n",ant.ID);
+                    List<Integer> temp = ant.visitedNodes;
+                    temp = optimizePath(ant.visitedNodes, connections);
+                    int tempLength = COLONY.length(temp, connections); // Laenge berechnen
 
                     // Bei kuerzerer Laenge die Laenge in bestLength abspeichern
                     // und die verwendete Route in bestRoute
-                    if (((bestLength - tempLength) > 0) || bestLength == 0) {
-                        bestLength = tempLength; // Neue kuerzeste Laenge
-                                                 // abspeichern
-                        bestRoute.addFirst(ant.visitedNodes); // Neue
-                                                                         // kuerzeste
-                                                                         // Route
-                                                                         // abspeichern
+                    if (BESTNUMBEROFDELIVERYVANS > ant.loadCount || (BESTNUMBEROFDELIVERYVANS == ant.loadCount && ((bestLength - tempLength) > 0) || bestLength == 0)) {
+                        BESTNUMBEROFDELIVERYVANS = ant.loadCount;
+                        bestLength = tempLength; // Neue kuerzeste Laenge abspeichern
+                        bestRoute.addFirst(temp); // Neue kuerzeste Route abspeichern
                         if (bestRoute.size() > MAXHISTORY) {
                             bestRoute.removeLast();
                         }
@@ -202,10 +236,112 @@ public class Simulation {
             connections = COLONY.evaporate(connections, RHO);
 
             // Nodes mit aktualisierten Connections erzeugen
-            nodes = Parser.initNodes(connections);
+            //TODO nodes = Parser.initNodes(connections);
         }
-        
+        //if(bestTours != null && !bestTours.isEmpty()){
+        if(bestRoute.isEmpty()){
+        	JOptionPane.showMessageDialog(null, "Noch keinen Weg gefunden,bitte mehr Steps ausführen!");
+        } else {
+            bestTours = calculateTours(bestRoute.getFirst());
+        }
+        //}
         return new Simulation(CITIES, TESTDATA, connections, antList, nodes, bestRoute, this.STEPS + steps);
     } // methode
 
+    /**
+     * Optimiert den Weg den die Ameise nimmt
+     * @param temp
+     * @param connections
+     * @return
+     */
+    List<Integer> optimizePath(List<Integer> temp, List<Connection> connections){
+        List<Integer> path = temp.subList(0, temp.size());
+        Ant ant = new Ant(ACOImpl.packages, ACOImpl.total);
+        for(int i = 0; i + 2 < path.size(); i++){
+            if(!(ant.load(path.get(i + 1)) || ant.unload(path.get(i + 1))) && 
+                    path.get(i) == path.get(i + 2)){
+                path.remove(i + 1);
+                path.remove(i + 1);
+                i--;
+                    }
+        }
+
+        for(int i = 0; i + 1 < path.size(); i++){
+            if(path.get(i) == path.get(i + 1)){
+                path.remove(i + 1);
+                i--;
+            }
+        }
+        ant = new Ant(ACOImpl.packages, ACOImpl.total);
+        int load = 0;
+        int unload1 = 0;
+        int unload2 = 0;
+        List<Integer> l = new ArrayList<Integer>();
+        l.add(path.get(0));
+        for(int i = 1; i < path.size(); i++){
+            if(ant.unload(path.get(i))){
+                if(unload1 == 0){
+                    unload1 = i;
+                    unload2 = i;
+                }else{
+                    unload2 = i;
+                }
+            }
+            if(ant.load(path.get(i)) || i == path.size() - 1){
+                if(COLONY.length(path.subList(load, unload2 + 1), connections) < COLONY.length(path.subList(unload2, i + 1), connections)){
+                    List<Integer> p = path.subList(load, unload2 + 1);
+                    for(int j = 1; j < p.size(); j++){
+                        l.add(p.get(j));
+                    }
+                    for(int j = p.size() - 2; j >= 0; j--){
+                        l.add(p.get(j));
+                    }
+                }else if(COLONY.length(path.subList(load, unload1 + 1), connections) > COLONY.length(path.subList(unload1, i + 1), connections)){
+                    List<Integer> p = path.subList(unload1, i + 1);
+                    for(int j = p.size() - 2; j >= 0; j--){
+                        l.add(p.get(j));
+                    }
+                    for(int j = 1; j < p.size(); j++){
+                        l.add(p.get(j));
+                    }
+                }else{
+                    List<Integer> p = path.subList(load + 1, i + 1);
+                    l.addAll(p);
+                }
+                unload1 = 0;
+                load = i;
+            }
+        }
+        path = l;
+        for(int i = 0; i + 1 < path.size(); i++){
+            if(path.get(i) == path.get(i + 1)){
+                path.remove(i + 1);
+                i--;
+            }
+        }
+        return path;
+    }
+	
+	/**
+     * berrechnet die einzelnen Touren einer Ameise
+     * @param path
+     * @return
+     */
+    ArrayList<List<Integer>> calculateTours(List<Integer> path) {
+        System.out.printf("calculateTours\n");
+        
+        ArrayList<List<Integer>> tours = new ArrayList<List<Integer>>();
+        Ant ant = new Ant(ACOImpl.packages, ACOImpl.total);
+        ant.tourFin = true;
+        int j = 0;
+        for (int i = 0; i < path.size(); i++) {
+            ant.unload(path.get(i));
+            if (ant.load(path.get(i)) || i == path.size() - 1) {
+                tours.add(path.subList(j, i + 1));
+                j = i;
+            }
+        }
+        System.out.printf("Finish calulating " + tours.size());
+        return tours;
+    }
 }
